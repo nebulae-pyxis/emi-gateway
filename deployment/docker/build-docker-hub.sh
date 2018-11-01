5d740a65fcd8f37262c538b5f09a68765f4b2e91
juansmolano <juansmolano@gmail.com>
2018-11-01 16:11:56 -0500

    set cpu & mem limit

--- deployment/docker/build-docker-hub.sh
+++ deployment/docker/build-docker-hub.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 export DOCKERHUB_IMAGE=emigateway
-export DOCKERHUB_TAG=1.0.2
+export DOCKERHUB_TAG=1.0.3
 
 rm -rf deployment/docker/emi-gateway/
 cp -R $API_SHELL_PATH/emi-gateway deployment/docker/emi-gateway
 
-docker build  -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:latest deployment/docker/
+docker build -m "300M" -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:latest deployment/docker/
 docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASS
 docker push $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG
 docker push $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:latest
